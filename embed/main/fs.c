16c1368929ed4403f1bf5c029327f69d1fbb2102
Jay Lang <jaytlang@mit.edu>
2023-06-18 17:11:27 -0400

    conn reset

--- embed/main/fs.c
+++ embed/main/fs.c
@@ -6,18 +6,18 @@
  * (c) jay lang, 2023
  * redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
+ *
  * 1. redistributions of source code must retain the above copyright notice,
  * this list of conditions and the following disclaimer.
- * 
+ *
  * 2. redistributions in binary form must reproduce the above copyright
  * notice, this list of conditions and the following disclaimer in the
  * documentation and/or other materials provided with the distribution.
- * 
+ *
  * 3. neither the name of the copyright holder nor the names of its
  * contributors may be used to endorse or promote products derived from this
  * software without specific prior written permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS “AS IS”
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -89,8 +89,10 @@ fs_slurp(char *fname, char **out, size_t *outsize)
 	fseek(f, 0, SEEK_SET);
 
 	if ((*out = calloc(*outsize, sizeof(char))) == NULL ||
-	    fread(*out, *outsize, sizeof(char), f) != *outsize)
+	    fread(*out, *outsize, sizeof(char), f) != *outsize) {
+		ESP_LOGI(TAG, "only read %zu bytes from file", *outsize);
 		rv = errno;
+	}
 
 	fclose(f);
 end:
@@ -110,7 +112,7 @@ fs_write_credentials(char *ssid, char *pass)
 		fclose(f);
 		remove(FS_SSIDPATH);
 		CATCH_RETURN(errno);
-	}	
+	}
 
 	fclose(f);
 
@@ -125,7 +127,7 @@ fs_write_credentials(char *ssid, char *pass)
 			remove(FS_SSIDPATH);
 			remove(FS_PASSPATH);
 			CATCH_RETURN(errno);
-		}	
+		}
 
 		fclose(f);
 	}
